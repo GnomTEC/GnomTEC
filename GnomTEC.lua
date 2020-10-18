18bfc33276deded9c2bffec65f26c39301bf3fd5
GnomTEC <info@gnomtec.de>
2020-10-19 00:33:57 +0200

    * Updated TOC for WoW patch 9.0.1 * Updated libraries: Ace3, libMSP * Changed Backdrop elements in GUI xml

--- GnomTEC.lua
+++ GnomTEC.lua
@@ -1,10 +1,10 @@
 ﻿-- **********************************************************************
 -- GnomTEC (GnomTEC Addon Framework)
--- Version: 8.2.5.16
+-- Version: 9.0.1.17
 -- Author: Peter Jack
 -- URL: http://www.gnomtec.de/
 -- **********************************************************************
--- Copyright © 2014-2019 by Peter Jack
+-- Copyright © 2014-2020 by Peter Jack
 --
 -- Licensed under the EUPL, Version 1.1 only (the "Licence");
 -- You may not use this work except in compliance with the Licence.
@@ -29,12 +29,12 @@ local L = LibStub("AceLocale-3.0"):GetLocale("GnomTEC_")
 local addonInfo = {
 	["Name"] = "GnomTEC (Addon Framework)",
 	["Description"] = L["L_DESCRIPTION"],	
-	["Version"] = "8.2.5.16",
-	["Date"] = "2019-12-22",
+	["Version"] = "9.0.1.17",
+	["Date"] = "2020-10-19",
 	["Author"] = "Peter Jack",
 	["Email"] = "info@gnomtec.de",
 	["Website"] = "https://www.gnomtec.de/",
-	["Copyright"] = "© 2014-2019 by Peter Jack",
+	["Copyright"] = "© 2014-2020 by Peter Jack",
 	["License"] = "European Union Public Licence (EUPL v.1.1)",
 	["FrameworkRevision"] = 13
 }
