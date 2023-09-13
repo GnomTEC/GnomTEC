3a856b8a469e5792d1311316a4d9ef443266ac16
Effie Flammfix <github@gnomtec.de>
2023-09-13 17:53:33 +0200

    * Updated TOC for WoW patch 10.1.7

--- GnomTEC.lua
+++ GnomTEC.lua
@@ -1,6 +1,6 @@
 ﻿-- **********************************************************************
 -- GnomTEC (GnomTEC Addon Framework)
--- Version: 10.1.5.21
+-- Version: 10.1.7.21
 -- Author: Peter Jack
 -- URL: http://www.gnomtec.de/
 -- **********************************************************************
@@ -29,8 +29,8 @@ local L = LibStub("AceLocale-3.0"):GetLocale("GnomTEC_")
 local addonInfo = {
 	["Name"] = "GnomTEC (Addon Framework)",
 	["Description"] = L["L_DESCRIPTION"],	
-	["Version"] = "10.1.5.21",
-	["Date"] = "2023-07-12",
+	["Version"] = "10.1.7.21",
+	["Date"] = "2023-09-13",
 	["Author"] = "Peter Jack",
 	["Email"] = "info@gnomtec.de",
 	["Website"] = "https://www.gnomtec.de/",
